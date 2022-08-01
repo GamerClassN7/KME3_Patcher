9870d1107b8cf617e47f5bfd8646624689e86011
JonatanRek <gamerclassn7@gmail.com>
2022-08-01 15:49:13 +0200

    Tweaks and changes

--- KME3_Patcher/MainWindow.xaml.cs
+++ KME3_Patcher/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.IO;
 using System.IO.Compression;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
 
 namespace KME3_Patcher
 {
@@ -27,6 +29,7 @@ namespace KME3_Patcher
         public MainWindow()
         {
             InitializeComponent();
+            ServerAddress.TextChanged += new TextChangedEventHandler(ServerAddress_TextChanged);
         }
         private void Window_ContentRendered(object sender, EventArgs e)
         {
@@ -73,8 +76,32 @@ namespace KME3_Patcher
             File.Copy(System.IO.Path.GetTempPath() + "\\hosts", hostfile, true);
         }
 
-        public static bool TestGameServer(string ipAddress = "127.0.0.1")
+        public static bool ValidateIP(string host)
         {
+            string ValidIpAddressRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+            
+            if (Regex.IsMatch(host, ValidIpAddressRegex))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool TestGameServer(IPAddress IpAddress)
+        {
+           /* try // 1
+            {
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                s.BeginConnect(IpAddress, 80); // 2
+                s.Shutdown(SocketShutdown.Both);
+            }
+            catch // 1
+            {
+                // not connected
+                return false;
+            }*/
+
             return true;
         }
 
@@ -86,40 +113,76 @@ namespace KME3_Patcher
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void PatchGame_Click(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
             openFileDlg.DefaultExt = "MassEffect3.exe";
             openFileDlg.Filter = "Mass Effect 3 (MassEffect3.exe)|MassEffect3.exe";
             Nullable<bool> result = openFileDlg.ShowDialog();
             if (result != true){
-                MessageBox.Show("Game nto found");
+                ServerStatusMessage.Content = "Game Not Found";
                 return;
             }
 
             string gameDirectoryPath = System.IO.Path.GetDirectoryName(openFileDlg.FileName);
 
+            ServerStatusMessage.Content = "Downloading Patch....";
             WebClient webClient = new WebClient();
             webClient.DownloadFile("https://github.com/Erik-JS/masseffect-binkw32/releases/download/r4/me3_binkw32.zip", System.IO.Path.GetTempPath() + "\\me3_binkw32.zip.zip");
 
+            ServerStatusMessage.Content = "Verifing Patch....";
             var checksum = GetMD5Checksum(System.IO.Path.GetTempPath() + "\\me3_binkw32.zip.zip");
             if (checksum != "4A838E04B9BEF86F99F3FC013B65C0BC") {
-                MessageBox.Show("checksum doesent match");
+                ServerStatusMessage.Content = "Verification Failed";
                 return;
             }
 
+            ServerStatusMessage.Content = "Appling Patch....";
             string zipPath = System.IO.Path.GetTempPath() + "\\me3_binkw32.zip.zip";
-
             CheckTempFile(gameDirectoryPath + "\\binkw32.dll");
             CheckTempFile(gameDirectoryPath + "\\binkw23.dll");
-
             ZipFile.ExtractToDirectory(zipPath, gameDirectoryPath);
 
+            ServerStatusMessage.Content = "Testing Server Conection....";
+            if (!TestGameServer(System.Net.IPAddress.Parse(ServerAddress.Text)))
+            {
+                ServerStatusMessage.Content = "Conection test Failed";
+                return;
+            }
+
+            ServerStatusMessage.Content = "Writing Redirection....";
             HostsFileAdd(ServerAddress.Text + " gosredirector.ea.com");
             HostsFileAdd(ServerAddress.Text + " kme.jacobtread.local");
 
-            MessageBox.Show(checksum);
-            ServerAddress.Text = checksum;
+            //MessageBox.Show(checksum
+            ServerStatusMessage.Content = "Done";
+        }
+
+        private void ServerAddress_TextChanged(object sender, EventArgs e)
+        {
+            if (ServerAddress.Text == "")
+            {
+                ServerStatusMessage.Content = "IP invalid";
+                return;
+            }
+
+            if (!ValidateIP(ServerAddress.Text))
+            {
+                ServerStatusMessage.Content = "IP invalid";
+                return;
+            }
+
+            ServerStatusMessage.Content = "Triing to conect to server";
+
+            if (!TestGameServer(System.Net.IPAddress.Parse(ServerAddress.Text)))
+            {
+                ServerStatusMessage.Content = "hostname/Ip is not walid";
+                return;
+            }
+
+            ServerStatusMessage.Content = "Server Valid";
+            PatchGame.IsEnabled = true;
+
         }
     }
 }
